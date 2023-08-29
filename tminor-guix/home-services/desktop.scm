7f1f792cfcc85fe72dab972f5bace953c76d842b
Thomas Minor <minort@gmail.com>
2023-08-29 12:12:51 -0700

    Rename module

--- tminor-guix/home-services/desktop.scm
+++ tminor-guix/home-services/desktop.scm
@@ -1,4 +1,4 @@
-(define-module (tassos-guix home-services desktop)
+(define-module (tminor-guix home-services desktop)
   #:use-module (gnu home services)
   #:use-module (gnu home services utils)
   #:use-module (gnu packages wm)
